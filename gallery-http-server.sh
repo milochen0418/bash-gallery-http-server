e607c00b5779085c3328fde78366dc67bb3fe6a3
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-17 11:08:41 +0000

    Update

--- gallery-http-server.sh
+++ gallery-http-server.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
 PORT_NUM=5566
-TOPIC=ALL
-SRC_DIR=/home/joanmirochen/nodejs/test/downloads 
+#TOPIC=ALL
+SRC_DIR=/home/joanmirochen/nodejs/test/downloads/校花
+
+
+# record current folder
+CURDIR=$(pwd)
+BACKDIR=""
+CURDIR_FP=/tmp/gallery-http-server-curdir.txt
+rm -f $CURDIR_FP
+echo $CURDIR > $CURDIR_FP
+
 
 cd /tmp/
+rm -rf gallery-web-generator
 git clone https://github.com/milochen0418/gallery-web-generator
 cd ./gallery-web-generator/
 #./gallery_procedure.sh 5566 /home/joanmirochen/nodejs/test/downloads ALL
-./gallery_procedure.sh $PORT_NUM $SRC_DIR $TOPIC
+./simple_gallery_procedure.sh $SRC_DIR $PORT_NUM
+
+
+
+# go back to current folder that you record before  
+BACKDIR=$(cat $CURDIR_FP)
+cd $BACKDIR
+exit 0
